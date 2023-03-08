9d3c0eef3da952279908784b7bba0c79385bdfdf
burak <design.burakk@gmail.com>
2023-03-08 12:09:36 +0300

    first commit by burak baba

---
+++ app/src/main/java/com/example/airplanemode/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.airplanemode
+
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver: AirplaneModeChangeReceiver
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        receiver = AirplaneModeChangeReceiver()
+
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            // registering the receiver
+            // it parameter which is passed in  registerReceiver() function
+            // is the intent filter that we have just created
+            registerReceiver(receiver, it)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
+}
